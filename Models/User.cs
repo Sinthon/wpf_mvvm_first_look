d7fba073394211e1c0eede7b510d7d77bf9349a0
Sinthon <s.sinthon@cas.com.kh>
2023-02-16 16:24:49 +0700

    add user_service

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace wpf_mvvm_first_look.Models
 {
-    internal class User
+    public class User : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Fields 
+        /// </summary>
+        /// 
+        private int userId { get; set; }
+        private string username { get; set; } = string.Empty;
+        private string password { get; set; } = string.Empty;
+        private string email { get; set; } = string.Empty;
+        private string phone { get; set; } = string.Empty;
+        private string address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int UserId { 
+            get => userId; 
+            set {
+                userId = value;
+                OnPropertyChanged("UserId");
+            }
+        }
+
+        public string Username { 
+            get => username;
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        public string Password { 
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        public string Email { 
+            get => email;
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        public string Phone { 
+            get => phone;
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+        public string Address { 
+            get => address;
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        /// <summary>
+        /// Functions
+        /// </summary>
+        /// 
+        public override bool Equals(object? obj)
+        {
+            if (obj is User)
+            {
+                return this.userId == ((User)obj).UserId;
+            }
+
+            return base.Equals(obj);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) 
+        { 
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
---
+++ Models/UserService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace wpf_mvvm_first_look.Models;
+
+public class UserService
+{
+    private static List<User>? ObjUserList { get; set; } = new();
+
+    public UserService() =>
+        ObjUserList = new List<User>()
+        {
+                new User() {
+                    UserId= 1,
+                    Username = "Sinthon",
+                    Email = "sinthon@example.com",
+                    Password = "P@ssw0rd",
+                    Address = "Siemreap",
+                    Phone = "081968806"
+                },
+        };
+
+    public static List<User>? GetObjUserList() => ObjUserList;
+
+    public static bool AddUser(User user)
+    {
+        if(user.Username == null)
+        {
+            throw new ArgumentException("User requred");
+        }
+
+        ObjUserList?.Add(user);
+        return true;
+    }
+
+    public static bool UpdateUser(User user)
+    {
+        var hasUser = ObjUserList?.Where(u=> u.Equals(user)).First();
+
+        if (hasUser == null)
+        {
+            return false;
+        }
+
+        hasUser = user;
+        return true;
+    }
+
+    public static bool DeleteUser(User user)
+    {
+        if(ObjUserList != null && !ObjUserList.Any(u => u.Equals(user)))
+        {
+            return false;
+        }
+
+        ObjUserList?.Remove(user);
+
+        return true;
+    }
+
+    public static List<User> Finds(User user)
+    {
+        return ObjUserList?.Where(u => u.Like(user)).ToList() ?? new();
+    }
+}
+
+
+
